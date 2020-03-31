b6974586ea6b8dde1e39f06f13464ef71ff38e0d
Marco Pagliari <lechuckcaptain@gmail.com>
2020-03-31 12:12:04 +0200

    Added basic test project

---
+++ CommonInterfaceTest/CommonInterfaceTest.swift
@@ -0,0 +1,24 @@
+//
+//  CommonInterfaceTest.swift
+//  CommonInterfaceTest
+//
+//  Created by Marco Pagliari on 31/03/2020.
+//  Copyright © 2020 Marco Pagliari. All rights reserved.
+//
+
+import XCTest
+
+class CommonInterfaceTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
